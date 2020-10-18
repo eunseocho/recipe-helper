582be1ce9e849a07517490f97aa76b71c50a1df6
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2020-10-17 23:32:43 -0400

    Add basic site layout

--- recipe-helper/src/App.js
+++ recipe-helper/src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#">Recipe Helper</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/recipes">My Recipes</Nav.Link>
+            <Nav.Link href="/addrecipe">Add a Recipe</Nav.Link>
+          </Nav>
+        </Navbar>
+        <div class="row justify-content-center">
+          <Router>
+            <Switch>
+              <Route path="/about">
+                <div class="pt-3 col-md-6">
+                  <About />
+                </div>
+              </Route>
+              <Route path="/recipes">
+                <div class="pt-3 col-md-4">
+                  <Recipes />
+                </div>
+              </Route>
+              <Route path="/addrecipe">
+                <div class="pt-3 col-md-4">
+                  <AddRecipe />
+                </div>
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    );
+  }
+}
+
+class About extends React.Component {
+  render() {
+    return (
+      <div class="text-center">
+        <h1> Welcome to Recipe Helper! </h1>
+        <p> This app is designed for HackGT 7! More information coming soon! </p>
+      </div>
+    );
+  }
+}
+
+class Recipes extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> This is the Recipes component! </p>
+      </div>
+    );
+  }
+}
+
+class AddRecipe extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> This is the AddRecipe component! </p>
+      </div>
+    );
+  }
 }
 
 export default App;
