1394331d2f11ae6dec1b9f4b386b6cca17e1b232
Eunseo Cho <echo99@gatech.edu>
2020-10-18 07:01:28 -0400

    added msgs for About

--- recipe-helper/src/routes/About.js
+++ recipe-helper/src/routes/About.js
@@ -3,9 +3,15 @@ import React from 'react';
 class About extends React.Component {
   render() {
     return (
-      <div className="text-center">
-        <h1> Welcome to Recipe Helper! </h1>
-        <p> This app is designed for HackGT 7! More information coming soon! </p>
+      <div className="text-center pt-5">
+        <h1> Welcome to NomNom! </h1>
+        <div className="pt-5">
+	        <p> NomNom is the simple,
+	        no-math solution for recipe and ingredient calculation. </p>
+	        <p> It provides a easy to use interface to input your recipes,
+	        and allows convenient storage and scaling of your recipes
+	        to any number of servings! </p>
+        </div>
       </div>
     );
   }
