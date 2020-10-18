f9bfb475de4f61c84115675fd5af007279fcc1a7
Eunseo Cho <echo99@gatech.edu>
2020-10-18 04:48:21 -0400

    Cards for showing recipes

--- recipe-helper/src/routes/ListRecipes.js
+++ recipe-helper/src/routes/ListRecipes.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 class ListRecipes extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {recipes : props.recipes}
+  }
+
   render() {
     return (
-      <div>
-        <p> This is the Recipes component! </p>
-      </div>
+      //<div>
+        //<p> This is the Recipes component! </p>
+      //</div>
+     <Card style={{ width: '18rem' }}>
+ 		   <ListGroup variant="flush">
+ 		     <ListGroup.Item>Cras justo odio</ListGroup.Item>
+  	   </ListGroup>
+	   </Card>
     );
   }
 }
